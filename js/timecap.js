803b93d5b08d48df2d3b3a1bd8b91a403be68d31
osabl <andrii.osabliuk@gmail.com>
2020-02-11 21:12:10 +0200

    add properties of date

--- js/timecap.js
+++ js/timecap.js
@@ -1,5 +1,9 @@
 class TimeCapture extends Date {
     constructor(...args) {
         super(...args);
+
+        this.day = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'][this.getDay()];
+        this.month = ['января', 'февраря', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'][this.getMonth()];
+        this.date = `${this.day}, ${this.getDate()} ${this.month}`;
     }
 }
