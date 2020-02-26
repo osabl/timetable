211bcdbe7859384d830351f3ef074aee7b03f04e
osabl <andrii.osabliuk@gmail.com>
2020-02-26 22:23:35 +0200

    add webpack & change project structure

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// JS
+import './js/script.js'
+
+// CSS
+import './assets/css/reset.css'
+import './assets/css/style.css'
+import './assets/css/slider.css'
+import './assets/css/header.css'
+import './assets/css/content.css'
+import './assets/css/progressbar.css'
+import './assets/css/checkbox.css'
+import './assets/css/background.css'
