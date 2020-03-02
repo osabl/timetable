0092745fea981279a1be835b858e96ac203e9c58
osabl <andrii.osabliuk@gmail.com>
2020-03-02 21:24:29 +0200

    change paths

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 // JS
-import './js/script.js'
+import './assets/js/index'
 
-// CSS
-import './assets/css/reset.css'
-import './assets/css/style.css'
-import './assets/css/slider.css'
-import './assets/css/header.css'
-import './assets/css/content.css'
-import './assets/css/progressbar.css'
-import './assets/css/checkbox.css'
-import './assets/css/background.css'
+// CSS | SCSS | SASS
+import './assets/scss/main.scss'
